cc783c2431bc34a657549bd7aec55aa807aaebf1
Michael Gosling <MGosling94@icloud.com>
2017-12-22 14:15:16 -0400

    Implemented 6 buttons linked to one function that calls an array based on the index of the button in the collection.

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -10,16 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+	var flipCount = 0 {
+		didSet {
+			flipCountLabel.text = "Flips: \(flipCount)"
+		}
+	}
+	
+	@IBOutlet var cardButtons: [UIButton]!
+	var emojiChoices = ["👻","🎃","🔥","🎃","👻","🔥"]
+	
+	
+	@IBOutlet weak var flipCountLabel: UILabel!
+	
+    @IBAction func touchCard(_ sender: UIButton) {
+		flipCount += 1
+		if let cardNumber = cardButtons.index(of: sender){
+			flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+		} else {
+			print("Chosen card was not in cardButtons")
+		}
+		
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+	
+	
+	func flipCard(withEmoji emoji: String, on button: UIButton) {
+		if button.currentTitle == emoji {
+			button.setTitle("", for: UIControlState.normal)
+			button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+		} else {
+			button.setTitle(emoji, for: UIControlState.normal)
+			button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+		}
+	}
+    
 
 }
 
